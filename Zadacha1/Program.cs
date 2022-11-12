71aeeb6e1e3b4da0e4796083c3717664aa85b8c2
Nikolay Kozlikin <kng15@mail.ru>
2022-11-12 04:06:38 +0300

    Comparison two nambers

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Запишите целое число ");
+int namberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Запишите второе целое число ");
+int namberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (namberOne > namberTwo)
+    Console.WriteLine("max = namberOne");
+else
+    Console.WriteLine("max = namberTwo");
