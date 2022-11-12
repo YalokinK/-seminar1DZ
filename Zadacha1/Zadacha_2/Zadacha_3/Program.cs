c757b1b7c39731816ca85a8c238b5933e46f5e7d
Nikolay Kozlikin <kng15@mail.ru>
2022-11-12 22:45:11 +0300

    Definition an even namber

---
+++ Zadacha1/Zadacha_2/Zadacha_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает число на вход и выдаёт, является оно четным или нет
+Console.WriteLine("Введите целое число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int result = namber % 2;
+
+if (result != 0)
+    Console.WriteLine(namber + " => нет");
+else
+    Console.WriteLine(namber + " => да");
