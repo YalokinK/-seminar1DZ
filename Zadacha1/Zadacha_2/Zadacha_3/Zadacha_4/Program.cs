71fd928f7838eecb2c77e005f526084f0a7fac76
YalokinK <116567421+YalokinK@users.noreply.github.com>
2022-11-13 23:19:26 +0300

    Delete Zadacha1 directory

--- Zadacha1/Zadacha_2/Zadacha_3/Zadacha_4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Напишите программу, которая принимает на вход число(N),а на выходе все четные числа от 1 до N  
-Console.WriteLine("Введите целое число: ");
-int namberN = Convert.ToInt32(Console.ReadLine());
-int namberA = 2;
-while (namberA <= namberN)
-{
-    Console.Write(namberA + " ");
-    namberA +=2;
-}
