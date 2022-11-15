6c4aa6e9edf5582921c726ad51b98160ef58bdca
Nikolay Kozlikin <kng15@mail.ru>
2022-11-16 00:45:33 +0300

    Task 10 added

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int randomNamber =  new Random().Next(100, 1000);
+
+Console.WriteLine(randomNamber);
+
+int dubleDigit = randomNamber / 10;
+int lastDigit = dubleDigit % 10;
+
+Console.WriteLine("Вторая цифра " + lastDigit);
+
