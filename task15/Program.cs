534e8bae44475d8352523d603cb2509727370969
Nikolay Kozlikin <kng15@mail.ru>
2022-11-16 19:07:15 +0300

    Fail task 15 added

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает на вход число,обозначающее день недели,и проверяет,являеься ли этот день выходным 
+Console.WriteLine("Введите число,соответсвующее дню недели: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+if (namber == 1 || namber == 2 || namber == 3 || namber == 4 || namber == 5)
+    Console.WriteLine("нет");
+
+else if (namber == 6 || namber == 7)
+    Console.WriteLine("Да");
+else
+Console.WriteLine("Такого дня недели нет");
+
