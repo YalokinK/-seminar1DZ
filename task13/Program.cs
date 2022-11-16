03eb4f749c60b3520bfa558f2866943c3d999209
Nikolay Kozlikin <kng15@mail.ru>
2022-11-16 16:55:23 +0300

    Fail task 13 added

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа выводит третью цифру заданного числа или сообщает, что этого числа нет
+
+Console.WriteLine("Введите любое целое число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+while (namber > 999)
+{
+    namber = namber / 10;
+} 
+if (99 < namber)
+    Console.WriteLine(namber % 10);
+else
+    Console.WriteLine("Третьей цифры нет");
