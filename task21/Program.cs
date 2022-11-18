b0dbc79a3f156b005285298ec4473a5fe736503f
Nikolay Kozlikin <kng15@mail.ru>
2022-11-19 01:46:04 +0300

    Fail task 21 added

---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа принимает на вход координаты двух точек и определяет расстояние между ними в 3D 
+// прострастве
+
+int aX = ReadInt("Введите координату X точки A: ");
+int aY = ReadInt("Введите координату Y точки A: ");
+int aZ = ReadInt("Введите координату Z точки A: ");
+int bX = ReadInt("Введите координату X точки B: ");
+int bY = ReadInt("Введите координату Y точки B: ");
+int bZ = ReadInt("Введите координату Z точки B: ");
+
+int sqrX = (aX - bX) * (aX - bX);
+int sqrY = (aY - bY) * (aY - bY);
+int sqrZ = (aZ - bZ) * (aZ - bZ);
+double distanse = Math.Sqrt(sqrX + sqrY + sqrZ);
+Console.WriteLine(distanse); 
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
