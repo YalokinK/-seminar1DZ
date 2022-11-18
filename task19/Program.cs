2dc2348104793e5630cfcfc22ef3c93d9f20ce72
Nikolay Kozlikin <kng15@mail.ru>
2022-11-18 22:52:02 +0300

    Fail task 19 added

---
+++ task19/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа принимает на вход 5-ти значное число и проверяет, является ли оно полиндроном:
+
+Console.WriteLine("Введите любое пятизначное число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+if (namber % 10  == namber / 10000 && namber /10 % 10 == namber / 1000 % 10)
+Console.WriteLine("Да");
+else
+Console.WriteLine("Нет");
