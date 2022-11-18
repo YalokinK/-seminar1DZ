ca73be80dfe3e4df3e53634a89859f5d56044c94
Nikolay Kozlikin <kng15@mail.ru>
2022-11-19 00:32:46 +0300

    Fail task 23 added

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа принимает на вход число N и выдает таблицу кубов от 1 до N:
+Console.WriteLine("Введите  целое число: ");
+
+int namber = Convert.ToInt32(Console.ReadLine());
+int namberA = 0;
+
+while (namberA < namber)
+{
+    namberA++;
+Console.WriteLine(namberA * namberA * namberA + "");
+}
