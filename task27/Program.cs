602c04dc77017768ded30894d6a447a1ea07a4cb
Nikolay Kozlikin <kng15@mail.ru>
2022-11-25 00:23:23 +0300

    Fail task 27 added

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа приримает на вход число и выдает сумму цифр в числе:
+
+Console.WriteLine("Введите любое целое число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (namber > 0)
+{
+    sum = sum + namber % 10;
+    namber = namber / 10;
+}
+Console.WriteLine($"Сумма цифр: {sum}");
