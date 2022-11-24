de4a46cb9a433b03fd92536b4bf5ff7eefe3c985
Nikolay Kozlikin <kng15@mail.ru>
2022-11-24 23:48:51 +0300

    Fail task 25 added

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит
+// число А в натуральную степень В:
+
+int namberA = new Random().Next(1, 10);
+int namberB = new Random().Next(1, 10);
+int result = 1;
+for (int count = 1; count <= namberB; count ++)
+{
+    result = result * namberA;
+}
+Console.WriteLine($"Число {namberA} в степени {namberB} равно " + result);
