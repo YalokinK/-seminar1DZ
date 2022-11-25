09aee92fcb40d4d0972f441e2dc17d451d1ab654
Nikolay Kozlikin <kng15@mail.ru>
2022-11-25 03:49:29 +0300

    Fail task 29 added

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа,которая задает массив из N элементов и выводит их на экран.
+// Вывод сделать отдельным методом.
+   
+Console.WriteLine("Введите любое положительное целое число(N): ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+PrintArray(array);
+Console.WriteLine();
+
+
