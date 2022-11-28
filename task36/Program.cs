2ec5100f0fb363aca19730d093718add95a05375
Nikolay Kozlikin <kng15@mail.ru>
2022-11-28 14:22:12 +0300

    Fail task 36 added

---
+++ task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив,заполненный случайными числами.
+// Найдите сумму элементов,стоящих на нечетных позициях.
+
+Console.WriteLine("Какой будет размер массива? Введите цифру: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int sum = 0;
+void FillArrayRandomNambers(int[] array)
+{
+    for (int i=0; i<size; i++)
+    array[i] = new Random().Next(-9, 10);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i=0; i<size; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+FillArrayRandomNambers(array);
+PrintArray(array);
+for (int j=1; j<size; j +=2)
+{
+    sum = sum + array[j];
+}
+Console.WriteLine();
+Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
