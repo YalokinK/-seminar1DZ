e92f22c63f10c32dc11986cb914dbb2fd47f6bbb
Nikolay Kozlikin <kng15@mail.ru>
2022-12-01 12:55:16 +0300

    Fail task 41 added

---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит М чисел.
+// Посчитайте,сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите целые числа через запятую: ");
+string nambersM = Console.ReadLine();
+int[] nambers = nambersM.Split(',').Select(int.Parse).ToArray();
+Console.WriteLine("\nМассив: {0}",string.Join(" ",nambers));
+int count = 0;
+for (int i=0; i < nambers.Length; i++)
+{
+    if (nambers[i] <= 0)
+        continue;
+    else
+        count++;
+}    
+Console.WriteLine("Положительных чисел: " + count);
+
