891395805683f989862f1a4a19e780e5c26788ef
Nikolay Kozlikin <kng15@mail.ru>
2022-12-12 22:53:15 +0300

    Fail task 66 added

---
+++ task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу,которая найдет
+// сумму натуральных элементов в промежутке от M до N.
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+if (M > N)
+{ 
+    int temp = M;
+    M = N;
+    N = temp;
+}
+int SumRec (int m, int n)
+{
+    if (m == n) 
+    return n;
+    else
+    {
+        return n + SumRec(m, n-1);
+    }
+}
+Console.WriteLine(SumRec(M, N));
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int namber = Convert.ToInt32(Console.ReadLine());
+    return namber;
+}
