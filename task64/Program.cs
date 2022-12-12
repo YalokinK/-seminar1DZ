f82d89a7299f5817e4440cbcebb54b10e8bc7dbc
Nikolay Kozlikin <kng15@mail.ru>
2022-12-12 23:47:50 +0300

    Fail task 64 added

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу,которая выведет все
+// натуральные числа от от N до 1. Выполнить с помощью рекурсии.
+
+int namber = ReadInt("Введите число N: ");
+Console.WriteLine(NaturalNamber(namber));
+
+int NaturalNamber(int n)
+{
+    if (n == 1) return 1;
+    else
+    {
+         Console.Write(n + ", ");
+        NaturalNamber(n-1);
+//        return 1;
+    }
+    return 1;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int namber = Convert.ToInt32(Console.ReadLine());
+    return namber;
+}
+
